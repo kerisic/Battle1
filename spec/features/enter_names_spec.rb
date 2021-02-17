816ce67f39c304d001097c730cb1078f890da918
Christine Chee <christinechee88@hotmail.com>
2021-02-17 18:31:05 +0000

    Extract repetitive code in rspec to webhelpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Chrissy'
-    fill_in :player_2_name, with: 'Hannah'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Chrissy vs. Hannah'
   end
 end
